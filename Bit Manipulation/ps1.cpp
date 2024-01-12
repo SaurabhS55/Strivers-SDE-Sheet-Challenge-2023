6f33020f8cbea0bbde8446917399bb9a486d76f0
Saurabh <saurabhsalunke91@gmail.com>
2024-01-12 22:52:32 +0530

    bit manipulation

---
+++ Bit Manipulation/ps1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    #ifdef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    int n;
+    cin>>n;
+    cout<<n;
+    return 0;
+}
